c9db1e732eb52b8296a2d14f1dbe795b3dd9ab39
Mariano-Ryser <rysermariano@gmail.com>
2022-01-16 23:23:28 +0100

    added Foto - Style -

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-
+// import MR2 from '../../assets/img/MR2.jpg'
+import HomeItems from './HomeItems'
 import './home.css'
 
 // function ich(data) {
@@ -19,8 +20,18 @@ class Home extends Component {
     render(){
         return (
             <div className="box-my-description">
-               <h3>Hi! my name is {data.firstName + " " + data.lastName}, <br></br><br></br> I am {data.age} years old,<br></br><br></br> and by profession,<br></br><br></br> {data.profession}.</h3>
+
+               <div className="description">
+               <h3>Hi! my name is {data.firstName + " " + data.lastName},
+                <br></br><br></br> I am {data.age} years old,<br></br><br></br>
+                 and by profession,<br></br><br></br> {data.profession}. <br></br> </h3>
+               </div>
+               
+               <div className="foto">
+                 
+               </div>
             </div>
+            
         )
     }
 }
