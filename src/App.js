66d4dd29555f6c0f900f4fc28850385adab5e4d2
Mariano-Ryser <rysermariano@gmail.com>
2022-01-23 17:55:36 +0100

    aczer+76

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar2 from './components/Navbar2/Navbar2';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import './App.css';
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Navbar2 />
+      <Navbar />
       <Home />
       <Footer />
 
