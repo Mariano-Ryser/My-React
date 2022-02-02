073431823c79e582633d0f6021412bef519ccd60
Mariano-Ryser <rysermariano@gmail.com>
2022-02-02 18:38:49 +0100

    Calendar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-
-import Footer from './components/Footer/Footer';
-
+import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import Calendar from './Calendar';
+
 
 function App() {
-  const [count, setCount] = useState(0);
-  
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      
-     <div className='container-eje'>
-     <h1 className="title">Hola aqui</h1>
-          <br></br>
-          <p className="p">Donde estaremos probando los Hooks de React
-          el primero es el UseState()</p>
-          <br></br>
-      <p>You clicked {count} times</p>
-        <br></br>
-         <button className='button' onClick={() => setCount(count + 1)}>Increment</button>
-      </div>
-
-      <Footer />
+      <Nav />
+      <Calendar />
     </div>
+ 
   );
 }
 
 export default App;
+
+
+
---
+++ src/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css'
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Monaten</h3>
+        <ul className='nav-links'>
+            <li>Januar</li>
+            <li>Februar</li>
+            <li>Marz</li>
+            <li>April</li>
+            <li>Mai</li>
+            <li>Juni</li>
+            <li>Juli</li>
+            <li>Agoust</li>
+            <li>September</li>
+            <li>October</li>
+            <li>November</li>
+            <li>December</li>
+        </ul>
+    </nav>
+ 
+  );
+}
+
+export default Nav;
+
