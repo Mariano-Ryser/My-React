b414844bf5df9269133b978a3699b7f0436ff69d
Mariano Ryser <rysermariano@gmail.com>
2022-02-28 10:44:30 +0100

    --btn

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import './App.css';
-import Nav from './Nav';
-import Calendar from './Calendar';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
+import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
-      <Nav />
-      <Calendar />
+      <Navbar />
+      <Home />
+      
+     <div className='container-eje'>
+     <h1 className="title">Hola aqui</h1>
+          <br></br>
+          <p className="p">Donde estaremos probando los Hooks de React
+          el primero es el UseState()</p>
+          <br></br>
+      <p>You clicked {count} times</p>
+        <br></br>
+         <button className='button' onClick={() => setCount(count + 1)}>Increment</button>
+         <button className='button' onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+
+      <Footer />
     </div>
- 
   );
 }
 
 export default App;
-
-
-
---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import './home.css';
+
+
+
+class Home extends Component {
+
+    render(){
+        return (
+         <div className="content flow">
+            <div className="grid-ish">
+                <div className="col">------</div>
+                <div className="col">------</div>
+                <div className="col">------</div>
+                <div className="col">------</div>
+                <div className="col">------</div>
+            </div>
+          
+        
+         </div>
+            
+        )
+    }
+}
+export default Home;
